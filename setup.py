768311e7ce8b5a9a6b375db9b910cc9e1d6d6911
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2020-10-09 16:39:14 -0300

    updating setup

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+LICENSE = (HERE / "LICENSE.md")#.read_text()
 
 setup(
     name="pyequion",
-    version='0.0.1',
+    version='0.0.5',
     description="Chemical equilibrium for electrolytes in pure python",
-    packages=["pyequion"],
+    # packages=["pyequion"],
+    packages=find_packages(exclude=("tests",)),
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Caio Marcellos",
-    license='BSD',
+    license='BSD 3-Clause',
+    license_file=LICENSE,
     email='caiocuritiba@gmail.com',
     include_package_data=True,
     zip_safe=False,
@@ -18,6 +30,20 @@ setup(
         'scipy',
         'sympy',
     ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords=[
+        "scientific",
+        "chemical equilibrium",
+    ]
     # package_data = {
     #     # If any package contains *.txt or *.rst files, include them:
     #     '': ['*.txt', '*.xml', '*.special', '*.huh'],
