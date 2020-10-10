b18813ee8ad20e9a35d2c1f4c094073be5bdf4dd
Caio F. Curitiba Marcellos <caiocuritiba@gmail.com>
2020-10-09 22:40:16 -0300

    sync

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 print('PATH', os.path.abspath('../../pyequion'))
 sys.path.insert(0, os.path.abspath('../../'))
-
+autodoc_mock_imports = ["numpy", "scipy", "sympy", "pyequion"]
 
 # -- Project information -----------------------------------------------------
 
